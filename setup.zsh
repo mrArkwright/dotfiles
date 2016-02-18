80eb749ceb686b4abb0f339bef9d5c8253d4346a
Jannik Theiß <dev@coldsun.org>
2016-02-18 18:52:35 +0100

    fixed

---
+++ setup.zsh
@@ -0,0 +1,26 @@
+#!/usr/bin/env zsh
+
+add_symlink() {
+	local src=$1
+	local dst=$2
+
+	if [[ ! -a $HOME/$dst ]]; then
+		if ln -s .dotfiles/$src $HOME/$dst; then
+			print -P "%{%F{green}%}✔%{%F{default}%} $dst successfully symlinked"
+		else
+			print -P "%{%F{red}%}✘%{%F{default}%} error symlinking $dst"
+		fi
+	else
+		print -P "%{%F{green}%}●%{%F{default}%} $dst already exists"
+	fi
+}
+
+# zsh
+add_symlink "zsh/zshrc" ".zshrc"
+
+#vim
+add_symlink "vim/vimrc" ".vimrc"
+
+#git
+add_symlink "git/gitconfig" ".gitconfig"
+
